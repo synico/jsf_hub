489b906efb40cf67db6527408176ff52453fcb83
nick@p50 <synico@qq.com>
2018-11-08 20:33:58 +0800

    D20181108

--- primefaces_demo/src/main/java/com/nick/Application.java
+++ primefaces_demo/src/main/java/com/nick/Application.java
@@ -2,16 +2,21 @@ package com.nick;
 
 import com.nick.domain.AssetInfo;
 import com.nick.repository.AssetInfoRepository;
+import com.nick.util.MyDatabaseConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
 import java.util.Iterator;
+import java.util.List;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class})
@@ -29,8 +34,15 @@ public class Application {
     public void init() {
         Iterator<AssetInfo> assetInfos;
         assetInfos = assetInfoRepository.findAll().iterator();
-        while(assetInfos.hasNext()) {
-            System.out.println(assetInfos.next());
-        }
+//        while(assetInfos.hasNext()) {
+//            System.out.println(assetInfos.next());
+//        }
+
+        List<AssetInfo> assetInfoList = assetInfoRepository.findByNameContaining("设备");
+        System.out.println("Size: " + assetInfoList.size());
+
+        Pageable pagination = new PageRequest(2, 10);
+        Page<AssetInfo> assetInfoPage = assetInfoRepository.findByNameContaining("设备", pagination);
+        System.out.println(assetInfoPage.getTotalElements());
     }
 }
--- primefaces_demo/src/main/java/com/nick/repository/AssetInfoRepository.java
+++ primefaces_demo/src/main/java/com/nick/repository/AssetInfoRepository.java
@@ -1,6 +1,8 @@
 package com.nick.repository;
 
 import com.nick.domain.AssetInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,8 @@ public interface AssetInfoRepository extends PagingAndSortingRepository<AssetInf
 //    @Query("select new com.nick.domain.AssetInfo(id, uid, name, departNum) from AssetInfo a, SharedAssetType b where a.sharedAssetType=b.id and b.swapName like %:swapname%")
     @Query(value = "select a.id, a.uid, a.name, a.depart_num from asset_info a join shared_asset_type b on a.shared_asset_type=b.id where b.swap_name like %:name%", nativeQuery = true)
     List<AssetInfo> findBySwapName(@Param("name") String name);
+
+    List<AssetInfo> findByNameContaining(String name);
+
+    Page<AssetInfo> findByNameContaining(String name, Pageable pageable);
 }
